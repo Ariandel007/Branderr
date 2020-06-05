b1052c3363a2bd9a54718e2ec5d6e5a0a0596ccc
BraulioUI <uri-b11@outlook.com>
2020-06-05 01:38:30 -0500

    corregir login

---
+++ NUnitTestSpectFlow/Features/SpecFlowFeature1.feature
@@ -0,0 +1,8 @@
+﻿Feature: SpecFlowFeature1
+	Validate functionality on login page of Application
+
+@mytag
+Scenario: Validate button login
+	Given Open the Chrome and launch the application	
+	When Enter the Email and Password
+	Then the result should be the user logged
---
+++ NUnitTestSpectFlow/Steps/SpecFlowFeature1Steps.cs
@@ -0,0 +1,39 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using TechTalk.SpecFlow;
+
+namespace NUnitTestSpectFlow.Steps
+{
+    [TestFixture]
+    [Binding]
+    public class SpecFlowFeature1Steps
+    {
+
+        IWebDriver driver = new ChromeDriver();
+        [Test]
+        [Given(@"Open the Chrome and launch the application")]
+        public void GivenOpenTheChromeAndLaunchTheApplication()
+        {
+            //ScenarioContext.Current.Pending();
+            driver.Manage().Window.Maximize();
+            driver.Navigate().GoToUrl("https://localhost:5001/Identity/Account/Login");
+        }
+        [Test]
+        [When(@"Enter the Email and Password")]
+        public void WhenEnterTheEmailAndPassword()
+        {
+            //ScenarioContext.Current.Pending();
+            driver.FindElement(By.Id("Input_Email")).SendKeys("braulio123@gmail.com");
+            driver.FindElement(By.Id("Input_Password")).SendKeys("Passw0rd$");
+        }
+        [Test]
+        [Then(@"the result should be the user logged")]
+        public void ThenTheResultShouldBeTheUserLogged()
+        {
+            //ScenarioContext.Current.Pending();
+            driver.FindElement(By.Id("login_button")).Click();
+        }
+    }
+}
